f48de76381147e99131fe41a4afd32731106e17d
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-30 17:17:18 +0530

    ADDED:test if three feet is given should return 1 yard

--- test/LenthTest.js
+++ test/LenthTest.js
@@ -6,4 +6,10 @@ describe('tests for length conversion',function(){
         inch12=length.unitConversion(12,length.Length.INCH);
         assert.equal(feet1,inch12);
     });
+
+    it('given 3 feet should return 1 yard',function(){
+        threeFeet=length.unitConversion(3,length.Length.FEET);
+        oneYard=length.unitConversion(1,length.Length.YARD);
+        assert.equal(oneYard,threeFeet);
+    })
 });
